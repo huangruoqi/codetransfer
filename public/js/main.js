7e15a76c6d35b8c2b26172399a30cdbd3f98dd80
Ruoqi Huang <huangruoqi374@gmail.com>
2021-05-26 15:36:37 -0700

    made it better

--- public/js/main.js
+++ public/js/main.js
@@ -14,16 +14,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const copySuccess = document.getElementById('copy-success');
 
     const copyTextHandler = () => {
-    const text = codeBlock.innerText;
+        const text = codeBlock.innerText;
 
 
-    navigator.clipboard.writeText(text).then(
-        () => {
-            copySuccess.classList.add('show-message');
-            setTimeout(() => {
-            copySuccess.classList.remove('show-message');
-        }, 2500);
-        }
-    );
+        navigator.clipboard.writeText(text).then(
+            () => {
+                copySuccess.classList.add('show-message');
+                setTimeout(() => {
+                copySuccess.classList.remove('show-message');
+            }, 2500);
+            }
+        );
     };
+    copyButton.addEventListener('click', copyTextHandler);
+
 })
