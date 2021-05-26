96ea92ef6e274d38392299d6ecc9e4324b85dc88
Ruoqi Huang <huangruoqi374@gmail.com>
2021-05-26 15:16:55 -0700

    made it better

--- public/js/main.js
+++ public/js/main.js
@@ -8,4 +8,22 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }, 2500);
+    const codeBlock = document.getElementById('code');
+    const copyButton = document.getElementById('copyButton');
+    const copySuccess = document.getElementById('copySuccess');
+    const copyTextHandler = () => {
+        const text = codeBlock.innerText;
+        navigator.clipboard.writeText(text).then{
+            () => {
+                copySuccess.classList.add('show-message');
+                setTimeout(() => {
+                    copySuccess.classList.remove('show-message');
+                }, 2500);
+            },
+            () => {
+                console.log('Error writing to the clipboard');
+            }
+        }
+    }
+    copyButton.addEventListener('click', copyTextHandler);
 })
