84d7b00ca71fd93a89e907bc679d874692b422b1
Ruoqi Huang <huangruoqi374@gmail.com>
2021-05-26 13:09:45 -0700

    made it better

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
 var isRefreshed = false;
 
@@ -10,6 +11,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 app.get('/',(req, res) => {
     fs.readFile('./code.txt', 'utf-8', (err, data)=> {
