120aefdfacc37558c110465e52de0f4b73d76270
Ruoqi Huang <huangruoqi374@gmail.com>
2021-05-27 12:15:06 -0700

    made it better

--- public/js/upload.js
+++ public/js/upload.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
         let code = editor.getValue();
         fetch('https://codetransfer.herokuapp.com/upload/input', {
             method: 'POST', 
-            body: JSON.stringify(code);
+            body: JSON.stringify(code)
         }).then(res => {
             if (res.redirected) {
                 window.location.href = '/';
