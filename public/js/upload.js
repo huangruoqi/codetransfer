8653a23c06770a382854f738a727c029c6899a97
Ruoqi Huang <huangruoqi374@gmail.com>
2021-05-27 12:13:24 -0700

    made it better

--- public/js/upload.js
+++ public/js/upload.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('upload-button').addEventListener('click', () => {
         let code = editor.getValue();
-        fetch('https://codetransfer.herokuapp.com/upload/input', {method: 'POST'}).then(res => {
+        fetch('https://codetransfer.herokuapp.com/upload/input', {
+            method: 'POST', 
+            body: JSON.stringify(code);
+        }).then(res => {
             if (res.redirected) {
                 window.location.href = '/';
             }
